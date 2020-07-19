3dfa58b8ff3f74412ffe41b1ebd0ac2497514464
Joseph Essin <josephessin@gmail.com>
2020-07-19 16:07:05 -0500

    Update template

--- jest.config.js
+++ jest.config.js
@@ -2,22 +2,14 @@ module.exports = {
 	preset: "ts-jest",
 	testEnvironment: "node",
 	testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/dist"],
-	collectCoverageFrom: [
-		"<rootDir>/src/**/*.ts"
-	],
+	collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
 	// Thanks to https://stackoverflow.com/a/50863753/4615448
-	roots: [
-		"<rootDir>",
-	],
-	modulePaths: [
-		"<rootDir>",
-	],
-	moduleDirectories: [
-		"node_modules"
-	],
-	"coverageReporters": [
-		"json-summary", 
-		"text",
-		"lcov"
-	]
+	roots: ["<rootDir>"],
+	modulePaths: ["<rootDir>"],
+	moduleDirectories: ["node_modules"],
+	coverageReporters: ["json-summary", "text", "lcov"],
+	// Parcel ~ alias for testing
+	moduleNameMapper: {
+		"~(.*)$": "<rootDir>/src/$1",
+	},
 };
